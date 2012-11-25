65a585f5dc88d0fc604eadc62207f189a2acb6c6
Luis Antunes <lantunes@gmail.com>
2012-11-25 08:25:04 -0500

    renaming files

---
+++ mojave-core/src/main/java/org/mojavemvc/exception/DefaultJSPErrorHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2011-2012 Mojavemvc.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mojavemvc.exception;
+
+import org.mojavemvc.FrontController;
+import org.mojavemvc.views.JSP;
+import org.mojavemvc.views.View;
+
+/**
+ * The default {@link ErrorHandler} for the application. Simply returns a
+ * {@link JSP} based on the 'jsp-error-file' init parameter (see
+ * {@link org.mojavemvc.FrontController}).
+ * 
+ * @author Luis Antunes
+ */
+public class DefaultJSPErrorHandler implements ErrorHandler {
+
+    public View handleError(Throwable e) {
+
+        return new JSP(FrontController.getJSPErrorFile());
+    }
+}
